07d31af0a1109f5bea9185c6a198eb246972ce6f
Aparna Paul <aparnapaul55@gmail.com>
2025-03-31 20:01:46 +1030

    Secure false

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,7 @@ export const loginUser = tryCatch(async (req, res) => {
     )
     // Store token in cookie
     res.cookie('token', jwtToken, { sameSite: "Lax",
-        secure: true,
+        secure: false,
         httpOnly: false, path: "/",
     });
 
@@ -74,7 +74,7 @@ export const signupUser = tryCatch(async (req, res) => {
 
     // Store token in cookie
     res.cookie('token', jwtToken, { sameSite: "Lax",
-        secure: true,
+        secure: false,
         httpOnly: false, path: "/",
     });
 
